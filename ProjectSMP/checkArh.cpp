c7a064dcc1ecd04d844e7ef558a951efaca8f542
Ufimtsev <matvey.ufimtsev@gmail.com>
2021-01-24 17:35:08 +0700

    modified:   ProjectSMP/checkArh.cpp 	modified:   ProjectSMP/maincpp.cpp

--- ProjectSMP/checkArh.cpp
+++ ProjectSMP/checkArh.cpp
@@ -4,17 +4,11 @@ uint32_t checkArh()
 {
     int res = 0;
     asm volatile(
-		"MOV EAX, 0x1	        \n\t"
+		"MOV EAX, 0x7	        \n\t"
         "XOR ECX, ECX           \n\t"
         "CPUID                  \n\t"
-        "MOV EAX, 1             \n\t"
-        "SHL EAX, 28            \n\t"
-        //"MOV ECX, EBX           \n\t"
-        "AND ECX, EAX           \n\t"
-        "SHL EAX, 12            \n\t"
-  //      "AND EBX, EAX           \n\t"
-  //      "ADD ECX, EBX            \n\t"
-        "MOV %0, ECX            \n\t"
+        "AND EBX, 0x20          \n\t"
+        "MOV %0, EBX            \n\t"
     	: "=r" (res));
     return res;
 }
--- ProjectSMP/maincpp.cpp
+++ ProjectSMP/maincpp.cpp
@@ -12,7 +12,7 @@ int main(int argv, char* argc[])
 {
 	uint32_t cpuid = checkArh();
 	cout << "CPUID" << "\t" << cpuid << endl;
-	if((cpuid & static_cast<word>((1 << 28))) == 0){
+	if(cpuid != 0x20){
 		cout << "CPU is not avalible" << endl;
 		return 1;
 	}
