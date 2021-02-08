ea2b7480f6977d3083325c6b02b0be9752055047
Ufimtsev Matvey <matvey.ufimtsev@gmail.com>
2021-02-09 02:50:04 +0700

    modified:   ProjectSMP/maincpp.cpp

--- ProjectSMP/maincpp.cpp
+++ ProjectSMP/maincpp.cpp
@@ -5,62 +5,42 @@
 #include "headers/testClass.h"
 #include "headers/checkArh.h"
 #include "headers/core_Mode.h"
-
-using namespace std;
+#include "headers/cmd_line.h"
 
 int main(int argv, char* argc[])
 {
 	const char* filenameInstr;
 	const char* filenameData;
-
+	char cmode;
+	int mode;
+	int ni;
 	if(checkArh() != 0x20){
-		cout << "Your CPU is not supported." << endl;
-		cout << "Your CPU should be support AVX2 instruction set." << endl;
-		cout << "Program is ended with return code: 1." << endl;
+		std::cout << "Your CPU is not supported." << std::endl;
+		std::cout << "Your CPU should be support AVX2 instruction set." << std::endl;
+		std::cout << "Program is ended with return code: 1." << std::endl;
 		return 1;
 	}
-
-	if(argv == 3){
-		filenameInstr = argc[1];
-		filenameData = argc[2];
+	if(cmd_line(argv, argc, &filenameInstr, &filenameData, &cmode, &ni))
+		return 0;
+	std::cout << cmode << std::endl;
+	switch(cmode){
+		case 'r': mode = STDMODE;	break;
+		case 'd': mode = DEBUGMODE;	break;
+		case 's': mode = STEPMODE;	break;
+		default: 
+			std::cout << "Unknown mode: " << cmode << std::endl;
+            std::cout << "Use -r"" mode for standart emulating" << std::endl;//run
+            std::cout << "Use -d mode for debug print emulating" << std::endl;//debug
+            std::cout << "Use -s mode for step by step emulating" << std::endl;//step by step
+			mode = STDMODE;
+			return 0;
 	}
-	else if(argv == 2){
-		filenameInstr = argc[1];
-		filenameData = "./dmem.dat";
-	}
-	else if(argv == 1){
-		filenameInstr = "../test_gen/test.smpasm.bin";
-		filenameData = "./dmem.dat";
-	}
-//	byte gpr = NREG;
-//	byte idr = NREG;
 	SMP_word data_size = DMSIZE;
 	SMP_word instr_size = IMSIZE;
 	SMP_word entry = 0;
 	
 	core MasterCore(entry, instr_size, data_size, filenameInstr, filenameData);
-	MasterCore.start(37, DEBUGMODE);
-/*
-	debugger CoreDebug;
-	CoreDebug.v = 0;
-	while (true)
-	{
-		MasterCore.test_start(static_cast<uint64_t>(SADD_TEST_INSTR_0));
-		CoreDebug.debugMode(MasterCore);
-		MasterCore.test_start(static_cast<uint64_t>(SADD_TEST_INSTR_1));
-		CoreDebug.debugMode(MasterCore);
-		MasterCore.test_start(static_cast<uint64_t>(SADD_TEST_INSTR_2));
-		CoreDebug.debugMode(MasterCore);
-		MasterCore.test_start(static_cast<uint64_t>(SADD_TEST_INSTR_3));
-		CoreDebug.debugMode(MasterCore);
-		MasterCore.test_start(static_cast<uint64_t>(BSWP_TEST_INSTR_4));
-		CoreDebug.debugMode(MasterCore);
-		MasterCore.test_start(static_cast<uint64_t>(RRX_TEST_INSTR_5));
-		CoreDebug.debugMode(MasterCore);
-		MasterCore.test_start(static_cast<uint64_t>(VSADDH_TEST_INSTR_8));
-		CoreDebug.debugMode(MasterCore);
-		if (true)
-			break;
-	}*/
+	MasterCore.start(ni, mode);
+
 	return 0;
 }
