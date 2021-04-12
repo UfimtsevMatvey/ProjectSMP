6002da38b7cc4f1c313a43a58cb2400764d934ed
UfimtsevMatvey <matvey.ufimtsev@gmail.com>
2021-04-12 12:25:45 +0300

    alu define

---
+++ RTL/define/alu.def.v
@@ -0,0 +1,27 @@
+`define ALU_ADD64   5'b00001
+`define ALU_ADC64   5'b00010
+`define ALU_ADD32   5'b00011
+`define ALU_ADD16   5'b00100
+`define ALU_ADD8    5'b00101
+`define ALU_SUB64   5'b00110
+`define ALU_RSB64   5'b00111
+`define ALU_SBC64   5'b01000
+`define ALU_SUB32   5'b01001
+`define ALU_SUB16   5'b01010
+`define ALU_SUB8    5'b01011
+
+`define ALU_AND     5'b01100
+`define ALU_EOR     5'b01101
+`define ALU_ORR     5'b01110
+`define ALU_TST     5'b01111
+`define ALU_TEQ     5'b10000
+`define ALU_CMP     5'b10001
+`define ALU_CMN     5'b10010
+`define ALU_MVN     5'b10011
+`define ALU_LSL     5'b10100
+`define ALU_LSR     5'b10101
+`define ALU_ASR     5'b10110
+`define ALU_RRX     5'b10111
+`define ALU_ROR     5'b11000
+`define ALU_BSWP    5'b11001
+`define ALU_SWR     5'b11010
