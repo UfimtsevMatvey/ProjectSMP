d2a78d7535684e00c814ccd1f52cdfa0626be0f6
UfimtsevMatvey <matvey.ufimtsev@gmail.com>
2021-07-17 05:23:07 +0300

    debugged shifting operations

--- RTL/simulation/start_adder.tcl
+++ RTL/simulation/start_adder.tcl
@@ -7,9 +7,18 @@ vlog   ../../testbench/testbench_adder.v  ../../alu/*.v  ../../alu/adder/*.v
 # open the testbench module for simulation
 vsim -novopt work.testbench_adder
 
-# add all testbench signals to time diagram
+# add signals to time diagram
 add wave sim:/testbench_adder/*
-
+add wave sim:/testbench_adder/DUT_alu/adder_l_en
+add wave sim:/testbench_adder/DUT_alu/adder_h_en
+add wave sim:/testbench_adder/DUT_alu/shift_code
+add wave sim:/testbench_adder/DUT_alu/shift_arg
+add wave sim:/testbench_adder/DUT_alu/shift_value
+add wave sim:/testbench_adder/DUT_alu/shift_carry_in
+add wave sim:/testbench_adder/DUT_alu/result_out
+add wave sim:/testbench_adder/DUT_alu/shift_en
+add wave sim:/testbench_adder/DUT_alu/shift_rdy
+add wave sim:/testbench_adder/DUT_alu/shift_res
 # run the simulation
 run -all
 
